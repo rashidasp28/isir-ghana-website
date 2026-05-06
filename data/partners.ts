d68e2edb9b15edcb31e94db5a74bbab8557f0ce8
rashidasp28 <rashidasp28@gmail.com>
2026-05-06 23:30:09 +0100

    Add ISIR Ghana partners data

---
+++ data/partners.ts
@@ -0,0 +1,27 @@
+export const partners = [
+  {
+    name: 'Eco Empower Alliance',
+    website: 'https://ecoempoweralliance.org/',
+    logo: '/images/partners/eco-empower-alliance.png',
+  },
+  {
+    name: 'Herpol Africa',
+    website: 'https://herpolafrica.org/',
+    logo: '/images/partners/herpol-africa.png',
+  },
+  {
+    name: 'GEM Ghana',
+    website: 'https://gemghana.org',
+    logo: '/images/partners/gem-ghana.png',
+  },
+  {
+    name: 'Masjid EAl Emaan',
+    website: '#',
+    logo: '/images/partners/masjid-eal-emaan.png',
+  },
+  {
+    name: 'STEM Rising Mind Foundation',
+    website: '#',
+    logo: '/images/partners/stem-rising-mind-foundation.png',
+  },
+]
