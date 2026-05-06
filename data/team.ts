2b27e6cf659e64d2a52470c3c907a1fe9e713e7e
rashidasp28 <rashidasp28@gmail.com>
2026-05-06 23:29:40 +0100

    Add ISIR Ghana team data

---
+++ data/team.ts
@@ -0,0 +1,72 @@
+export const teamMembers = [
+  {
+    name: 'Abdul-Rashid Iddi',
+    role: 'Executive Director and Co-Founder',
+    image: '/images/team/abdul-rashid-iddi.png',
+  },
+  {
+    name: 'Kisa Owusu Ansah',
+    role: 'Deputy Director',
+    image: '/images/team/kisa-owusu-ansah.png',
+  },
+  {
+    name: 'Bashiru Aminu',
+    role: 'Programs Manager',
+    image: '/images/team/bashiru-aminu.png',
+  },
+  {
+    name: 'Mubarik H. Iddrisu',
+    role: 'Monitoring, Evaluation, Accountability and Learning Lead',
+    image: '/images/team/mubarik-h-iddrisu.png',
+  },
+  {
+    name: 'Yasmin Fatai',
+    role: 'Project Manager, Education',
+    image: '/images/team/yasmin-fatai.png',
+  },
+  {
+    name: 'Iren Assina Gai',
+    role: 'Project Manager, Agriculture',
+    image: '/images/team/iren-assina-gai.png',
+  },
+  {
+    name: 'Tamimu Mustapha',
+    role: 'Project Manager, Climate Resilience',
+    image: '/images/team/tamimu-mustapha.png',
+  },
+  {
+    name: 'Hawa Ibrahim',
+    role: 'Project Manager, Health',
+    image: '/images/team/hawa-ibrahim.png',
+  },
+  {
+    name: 'Dr. Manfred Anim',
+    role: 'Partnerships and Grant Manager',
+    image: '/images/team/manfred-anim.png',
+  },
+  {
+    name: 'Khadija Ibrahim',
+    role: 'Volunteer, Education',
+    image: '/images/team/khadija-ibrahim.png',
+  },
+  {
+    name: 'Kennedy',
+    role: 'Web Volunteer',
+    image: '/images/team/kennedy.png',
+  },
+  {
+    name: 'Emmanuel',
+    role: 'Web Volunteer',
+    image: '/images/team/emmanuel.png',
+  },
+  {
+    name: 'Abdul-Majeed Yakubu',
+    role: 'Volunteer',
+    image: '/images/team/abdul-majeed-yakubu.png',
+  },
+  {
+    name: 'Rebecca',
+    role: 'Volunteer, Health',
+    image: '/images/team/rebecca.png',
+  },
+]
